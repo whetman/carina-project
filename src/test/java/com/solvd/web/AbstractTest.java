dda2d0f2f9e7351010d8f81546799870257abe36
whetman <whetman@solvd.com>
2024-05-27 16:12:47 +0200

    parallel testing with chrome and firefox

--- src/test/java/com/solvd/web/AbstractTest.java
+++ src/test/java/com/solvd/web/AbstractTest.java
@@ -2,12 +2,19 @@ package com.solvd.web;
 
 import com.solvd.gui.pages.common.HomePageBase;
 import com.zebrunner.carina.core.IAbstractTest;
+import com.zebrunner.carina.utils.R;
+import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public abstract class AbstractTest implements IAbstractTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractTest.class);
+
+    private WebDriver driver;
 
     public HomePageBase openHomePage() {
         LOGGER.info("Opening HomePage");
@@ -15,6 +22,19 @@ public abstract class AbstractTest implements IAbstractTest {
         HomePageBase homePage = initPage(getDriver(), HomePageBase.class);
         homePage.open();
         return homePage;
+    }
+
+    @BeforeClass
+    @Parameters({"browser"})
+    public void setUp(String browser) {
+        R.CONFIG.put("capabilities.browserName", browser);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
 }
